c87f7f636ab0cd762c516b5c4cb178adffe6bf3f
Eduardo <dukahenrick06@gmail.com>
2024-11-01 09:01:18 -0300

    bb

--- jump/MainPage.xaml.cs
+++ jump/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace jump;
+﻿using System.Collections;
+
+namespace jump;
 
 public partial class MainPage : ContentPage
 {
@@ -8,6 +10,51 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+	bool estaMorto = false;
+	bool estaPulando = false;
+	const int tempoEntreFrames = 25;
+
+	int velocidade1 = 0;
+	int velocidade2 = 0;
+	int velocidade3 = 0;
+	int velocidade = 0;
+	int larguraJanela = 0;
+	int alturaJanela = 0;
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		CorrigeTamanhoCenario(w, h);
+		CalculaVelocidade(w);
+	}
+
+	void CalculaVelocidade(double w)
+	{
+		velocidade1 = (int)(w * 0.001);
+		velocidade2 = (int)(w * 0.004);
+		velocidade3 = (int)(w * 0.008);
+		velocidade = (int)(w * 0.01);
+	}
+
+	void CorrigeTamanhoCenario(double w, double h)
+	{
+		foreach (var a in stack)
+			(a as Image).WidthRequest = w;
+		foreach (var a in stack1)
+			(a as Image).WidthRequest = w;
+		foreach (var a in stack2)
+			(a as Image).WidthRequest = w;
+		foreach (var a in stack3)
+			(a as Image).WidthRequest = w;
+
+			stack. WidthRequest =w *1.5;
+	stack1.WidthRequest =w *1.5;
+	stack2.WidthRequest =w *1.5;
+	stack3.WidthRequest =w *1.5;
+
+	}
+
 	
+
 }
 
